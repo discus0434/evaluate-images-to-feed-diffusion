dada29b4679f3d6fb21f6f0c30ebd9239cf0af91
discus0434 <discus0434@gmail.com>
2022-11-10 15:28:12 +0000

    fix

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ git clone https://github.com/CompVis/latent-diffusion.git
 cd latent-diffusion
 conda run -n sd pip install -e .
 
-cd /eval_images_train_difficulty
+cd /evaluate-images-to-feed-diffusion
 wget -O ./model/waifu-diffusion-v1-4/kl-f8-anime2.ckpt https://huggingface.co/hakurei/waifu-diffusion-v1-4/resolve/main/vae/kl-f8-anime2.ckpt
