d533730387817cef28133039aff40863f8588932
discus0434 <discus0434@gmail.com>
2022-11-11 02:02:10 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
-conda run -n sd pip install -e git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers
-conda run -n sd pip install -e git+https://github.com/openai/CLIP.git@main#egg=clip
-
 conda run -n sd pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 \
     -f https://download.pytorch.org/whl/cu113/torch_stable.html
 
 cd src
+
+git clone https://github.com/openai/CLIP.git
+conda run -n sd pip install -e clip/.
+
+git clone https://github.com/CompVis/taming-transformers.git
+conda run -n sd pip install -e taming-transformers/.
+
 git clone https://github.com/CompVis/latent-diffusion.git
+conda run -n sd pip install -e latent-diffusion/.
 
-cd latent-diffusion
-conda run -n sd pip install -e .
-
-cd /evaluate-images-to-feed-diffusion
+cd ..
 wget -O ./model/waifu-diffusion-v1-4/kl-f8-anime2.ckpt \
     https://huggingface.co/hakurei/waifu-diffusion-v1-4/resolve/main/vae/kl-f8-anime2.ckpt
